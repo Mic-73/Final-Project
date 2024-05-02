51f264524ea6d9dd17fa0289836eabef7dd9e2d5
Michael Wood <mw746845@sju.edu>
2024-05-01 20:30:59 -0400

    update find path

--- FindPath.py
+++ FindPath.py
@@ -4,6 +4,7 @@
 ####
 #### AI, Spring 2024
 #######################################################
+import queue
 import sys
 import re
 import tkinter as tk
@@ -66,19 +67,17 @@ class MazeGame:
                 else:
                     self.cells[i][j].priority = 0
 
-        delivery_locations = PriorityQueue()
+        self.delivery_locations = PriorityQueue()
 
         for i in self.delivery:
-            delivery_locations.put((self.cells[i[0]][i[1]].priority, i))
+            self.delivery_locations.put((self.cells[i[0]][i[1]].priority, i))
 
 
         #### Start state: (0,0) or top left
         self.agent_pos = eval(self.start)
 
         #### Goal state:  (rows-1, cols-1) or bottom right
-        print(delivery_locations.get())
-        print(delivery_locations.get())
-        self.goal_pos = self.delivery[1]
+        self.goal_pos = (0,0)
 
         self.goals_completed = []
         self.goals_failed = []
@@ -89,7 +88,7 @@ class MazeGame:
             self.cells[self.agent_pos[0]][self.agent_pos[1]].h = self.a_star_heuristic(self.agent_pos)
             self.cells[self.agent_pos[0]][self.agent_pos[1]].f = self.a_star_heuristic(self.agent_pos)
 
-        if (self.algorithm=="dijkstra"):
+        if (self.algorithm=="Dijkstra's"):
             self.cells[self.agent_pos[0]][self.agent_pos[1]].h = self.dijkstra_heuristic(self.agent_pos)
             self.cells[self.agent_pos[0]][self.agent_pos[1]].f = self.dijkstra_heuristic(self.agent_pos)
 
@@ -197,47 +196,67 @@ class MazeGame:
     #### Algorithm
     ############################################################
     def find_path(self):
-        open_set = PriorityQueue()
 
-        #### Add the start state to the queue
-        open_set.put((0, self.agent_pos))
+        while not self.delivery_locations.empty():
+            self.goal_pos = self.delivery_locations.get()[1]
 
-        #### Continue exploring until the queue is exhausted
-        while not open_set.empty():
-            current_cost, current_pos = open_set.get()
-            current_cell = self.cells[current_pos[0]][current_pos[1]]
+            print(self.goal_pos)
+            print(self.agent_pos)
 
-            #### Stop if goal is reached
-            if current_pos == self.goal_pos:
-                self.reconstruct_path()
-                break
+            open_set = PriorityQueue()
 
-            #### Agent goes E, W, N, and S, whenever possible
-            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                new_pos = (current_pos[0] + dx, current_pos[1] + dy)
+            #### Add the start state to the queue
+            open_set.put((0, self.agent_pos))
 
-                if 0 <= new_pos[0] < self.rows and 0 <= new_pos[1] < self.cols and not self.cells[new_pos[0]][
-                    new_pos[1]].is_wall:
+            #### Continue exploring until the queue is exhausted
+            while not open_set.empty():
+                current_cost, current_pos = open_set.get()
+                current_cell = self.cells[current_pos[0]][current_pos[1]]
 
-                    #### The cost of moving to a new position is 1 unit
-                    new_g = current_cell.g + 1
+                #### Stop if goal is reached
+                if current_pos == self.goal_pos:
+                    self.agent_pos = self.goal_pos
+                    self.reconstruct_path()
+                    print(self.goal_pos)
+                    print(self.agent_pos)
+                    while not open_set.empty():
+                        _, current_cell = open_set.get()
+                        self.cells[current_cell[0]][current_cell[1]].g = float("inf")
+                        self.cells[current_cell[0]][current_cell[1]].f = float("inf")
+                        self.cells[current_cell[0]][current_cell[1]].h = 0
+                    while not open_set.empty():
+                        # clear the list to make it possible to go backwards
+                        open_set.get()
+                    break
 
-                    if new_g < self.cells[new_pos[0]][new_pos[1]].g:
-                        ### Update the path cost g()
-                        self.cells[new_pos[0]][new_pos[1]].g = new_g
 
-                        ### Update the heurstic h()
-                        if (self.algorithm=="A*"):
-                            self.cells[new_pos[0]][new_pos[1]].h = self.a_star_heuristic(new_pos)
-                        if (self.algorithm=="Dijkstra"):
-                            self.cells[new_pos[0]][new_pos[1]].h = self.dijkstra_heuristic(new_pos)
+                #### Agent goes E, W, N, and S, whenever possible
+                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    new_pos = (current_pos[0] + dx, current_pos[1] + dy)
 
-                        ### Update the evaluation function for the cell n: f(n) = g(n) + h(n)
-                        self.cells[new_pos[0]][new_pos[1]].f = new_g + self.cells[new_pos[0]][new_pos[1]].h
-                        self.cells[new_pos[0]][new_pos[1]].parent = current_cell
+                    if 0 <= new_pos[0] < self.rows and 0 <= new_pos[1] < self.cols and not self.cells[new_pos[0]][
+                        new_pos[1]].is_wall:
 
-                        #### Add the new cell to the priority queue
-                        open_set.put((self.cells[new_pos[0]][new_pos[1]].f, new_pos))
+                        #### The cost of moving to a new position is 1 unit
+                        new_g = current_cell.g + 1
+
+                        if new_g < self.cells[new_pos[0]][new_pos[1]].g:
+                            ### Update the path cost g()
+                            self.cells[new_pos[0]][new_pos[1]].g = new_g
+
+                            ### Update the heurstic h()
+                            if (self.algorithm=="A*"):
+                                self.cells[new_pos[0]][new_pos[1]].h = self.a_star_heuristic(new_pos)
+                            if (self.algorithm=="Dijkstra's"):
+                                self.cells[new_pos[0]][new_pos[1]].h = self.dijkstra_heuristic(new_pos)
+
+                            ### Update the evaluation function for the cell n: f(n) = g(n) + h(n)
+                            self.cells[new_pos[0]][new_pos[1]].f = new_g + self.cells[new_pos[0]][new_pos[1]].h
+                            self.cells[new_pos[0]][new_pos[1]].parent = current_cell
+
+                            #### Add the new cell to the priority queue
+                            open_set.put((self.cells[new_pos[0]][new_pos[1]].f, new_pos))
+
 
     ############################################################
     #### This is for the GUI part. No need to modify this unless
